836f4cc90bcfcc1d3b279a697c803b4df8e42a7c
ZakiyDaulay <zakiydaulay@gmail.com>
2025-03-22 22:30:37 +0700

    added transaction history and routed to homepage

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY_CHOICES = [
+    ("Food", "Food"),
+    ("Housing", "Housing"),
+    ("Transportation", "Transportation"),
+    ("Health", "Health"),
+    ("Entertainment", "Entertainment"),
+    ("Shopping", "Shopping"),
+    ("Bills", "Bills"),
+    ("Travel", "Travel"),
+    ("Education", "Education"),
+    ("Finance", "Finance"),
+    ("Donations", "Donations"),
+    ("Miscellaneous", "Miscellaneous"),
+    ("Income", "Income"),
+]
+
+TRANSACTION_TYPE_CHOICES = [
+    ("expense", "Expense"),
+    ("income", "Income"),
+]
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.transaction_type} - {self.amount}"
