e423ebe8e21285c2c240fc864e71baf9a2500a26
ZakiyDaulay <zakiydaulay@gmail.com>
2025-03-22 21:51:29 +0700

    Saving my local changes before rebase

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,transaction_history
 
 urlpatterns = [
-    path("home/", home, name="tracker_home"),  # URL pattern for home
+    path("home/", home, name="tracker_home"),  # URL pattern for home,
+    path("transactions/", transaction_history, name="transaction_history")
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
+from .forms import TransactionForm
 
 # Create your views here.
 def home(request):
     return render(request, 'tracker/home.html')
+
+
+@login_required
+def transaction_history(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by("-date")
+
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect("transaction_history")
+    else:
+        form = TransactionForm()
+
+    return render(request, "tracker/transaction_history.html", {"transactions": transactions, "form": form})
