bb92de76561e4f89c5f21d8d07a779e36d85819f
James Lim <63968449+LimJames180@users.noreply.github.com>
2025-03-22 12:37:19 -0400

    pushing

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect
@@ -5,8 +6,25 @@ from django.contrib.auth.decorators import login_required
 from .models import Transaction
 from .forms import TransactionForm
 # Create your views here.
+
+@login_required
 def home(request):
-    return render(request, 'tracker/home.html')
+    total_spending = Transaction.objects.filter(user=request.user, transaction_type='expense').aggregate(total_spending=Sum('amount'))['total_spending'] or 0.00
+    total_earnings = Transaction.objects.filter(user=request.user, transaction_type='income').aggregate(total_earnings=Sum('amount'))['total_earnings'] or 0.00
+    budget_left = total_earnings - total_spending
+
+    # Fetch data for the charts
+    spending_data = list(Transaction.objects.filter(user=request.user, transaction_type='expense').values_list('amount', flat=True))
+    earnings_data = list(Transaction.objects.filter(user=request.user, transaction_type='income').values_list('amount', flat=True))
+
+    context = {
+        'total_spending': total_spending,
+        'total_earnings': total_earnings,
+        'budget_left': budget_left,
+        'spending_data': spending_data,
+        'earnings_data': earnings_data,
+    }
+    return render(request, 'tracker/home.html', context)
 
 
 @login_required
